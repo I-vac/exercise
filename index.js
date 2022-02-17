96a0f25d16fe5caa7aa4a7a633880ec4196c52c8
I-vac <ivo_nikolov00@abv.bg>
2022-02-17 08:33:33 +0200

    exercise 2

--- index.js
+++ index.js
@@ -1,48 +1,94 @@
-var name = "Ivaylo";
+// var name = "Ivaylo";
+//
+// let title = "Rush Hour";
+// // console.log(title);
+//
+// title = "Rush Hour 2";
+// // console.log(title);
+//
+// const ip_address_server_1415 = "192.168.0.1/255.255.255.0";
+// // console.log(ip_address_server_1415)
+//
+// let array = [
+//     title,
+//     ip_address_server_1415,
+//     true
+// ]
+// // console.log(array)
+//
+// let object = {
+//     title: "Borat",
+//     ip_address_server_1415: "122.256.0.121/255.255.255.0"
+// }
+// // console.log(object)
+// //cikli
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+//
+// for (let element of array) {
+//     console.log(element);
+// }
+//
+// let i = 0;
+// while (i< array.length) {
+//     console.log(array[i]);
+//     i++;
+// }
+//
+// //funkcii
+// function func(){
+//     console.log("executed the function")
+// }
+// func();
+//
+// const secondFunction = () => {
+//     console.log("executed the function")
+// }
+// secondFunction();
 
-let title = "Rush Hour";
-// console.log(title);
 
-title = "Rush Hour 2";
-// console.log(title);
 
-const ip_address_server_1415 = "192.168.0.1/255.255.255.0";
-// console.log(ip_address_server_1415)
+//VTORO UPRAJNENIE
 
-let array = [
-    title,
-    ip_address_server_1415,
-    true
+let data = [
+    10,
+    7,
+    25,
+    10,
+    56
 ]
-// console.log(array)
 
-let object = {
-    title: "Borat",
-    ip_address_server_1415: "122.256.0.121/255.255.255.0"
-}
-// console.log(object)
-//cikli
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-}
+// const modifiedArray = data.map((element)=> {
+//     if (element === 56) {
+//         element = 17;
+//     }
+//     return element;
+// })
+// console.log(modifiedArray);
 
-for (let element of array) {
-    console.log(element);
-}
+// const modifiedArray = []
+// for (let element of data) {
+//     if (element === 7) {
+//         element = 17;
+//     }
+//     modifiedArray.push(element);
+// }
+// console.log(modifiedArray);
 
-let i = 0;
-while (i< array.length) {
-    console.log(array[i]);
-    i++;
-}
+// const filteredData = data.filter((element)=>{
+//     if (element <= 25) {
+//         return element;
+//     }
+// })
+// console.log(filteredData);
 
-//funkcii
-function func(){
-    console.log("executed the function")
-}
-func();
+// const findData = data.find((element) => {
+//     if (element === 10) {
+//         return element;
+//     }
+// })
+// console.log(findData)
 
-const secondFunction = () => {
-    console.log("executed the function")
-}
-secondFunction();
+
+
